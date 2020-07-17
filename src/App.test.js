42586b5097184307a25372cfb63dabad3801584a
Codyt11 <Codyt11@hotmail.com>
2020-07-16 23:12:09 -0500

    MVP in progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,34 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
+import ContactForm from './components/ContactForm'
+import { act } from "react-dom/test-utils";
 
-test("renders App without crashing", () => {
-  render(<App />);
-});
+
+test('ContactForm adds new users to the list', () => {
+  act(() => {
+    render(<ContactForm />);
+  })
+
+  //Grab the text Inputs
+  const firstNameInput = screen.getByText(/first name/i);
+  const lastNameInput = screen.getByText(/last name/i);
+  const emailInput = screen.getByText(/email/i);
+  const messageInput = screen.getByText(/message/i);
+
+  //Grab the submit inputs
+  const submitButton = screen.getByRole('button', /submit/i)
+
+  act(() => {
+    fireEvent.change(firstNameInput, { target: { ref: 'Cody' } })
+    fireEvent.change(lastNameInput, { target: { ref: 'Townley' } })
+    fireEvent.change(emailInput, { target: { ref: 'Cody@Townley.com' } })
+    fireEvent.change(messageInput, { target: { ref: 'test message' } })
+
+    //Fire the submit event
+    fireEvent.click(submitButton)
+  })
+  expect.objectContaining(expect.anything())
+})
+
+
